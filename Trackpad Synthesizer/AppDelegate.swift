42bc3e8a26e7e2e616563edfec685f0ead82ea93
Victor Wang <victor@Victors-iMac.local>
2016-12-04 11:36:27 -0800

    Initial Commit

---
+++ Trackpad Synthesizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Trackpad Synthesizer
+//
+//  Created by Victor Wang on 12/4/16.
+//  Copyright © 2016 Victor Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Trackpad Synthesizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trackpad Synthesizer
+//
+//  Created by Victor Wang on 12/4/16.
+//  Copyright © 2016 Victor Wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
